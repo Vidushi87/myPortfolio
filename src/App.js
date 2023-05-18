c647f0afcfd85df859afbc8fa7eb03eeba0d2bc7
vpandey6 <vpandey6@partner.jaguarlandrover.com>
2023-05-18 12:49:55 +0100

    try

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <h5 style={{marginRight: "10px"}}>Vidushi Pandey </h5>    
+        <h6> | Front End Developer</h6>
+        </div>
+        <div id = "test"> 
+        <p>My Resume</p>
+        </div>
       </header>
     </div>
   );
